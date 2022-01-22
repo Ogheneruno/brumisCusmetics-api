a75be212b087f23ada219e62965544e53d02617a
Ogheneruno <ogefereogheneruno@gmail.com>
2022-01-22 10:51:15 +0100

    mailer.js

--- utils/welcomeEmail/welcomeEmail.js
+++ utils/welcomeEmail/welcomeEmail.js
@@ -1,4 +1,4 @@
-const sendEmail = require('../config/mailer');
+const sendEmail = require('../../config/mailer');
 
 const welcomeEmail = async (req, username, email, secretToken) => {
     const html = `
@@ -7,8 +7,6 @@ const welcomeEmail = async (req, username, email, secretToken) => {
         <br/>
         You can copy and paste the following token <strong>${secretToken}</strong> in the confirmation page
         or click on the link below to activate you account.
-        <br/><br/>
-        confirmation Link: https://${req.headers.host}/user/confirm-account/${secretToken}
         <br/>
         <br/>
         BRUMIS Cosmetics, bringing your skin the best experience!!!!!!!!
